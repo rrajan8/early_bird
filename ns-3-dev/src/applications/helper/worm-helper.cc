fe6d4fc8b6e468c56518c1b5fbcc85c869370a26
rrajan <rrajan8@gatech.edu>
2015-04-26 17:40:26 -0400

    added files to be edited for hack

---
+++ ns-3-dev/src/applications/helper/worm-helper.cc
@@ -0,0 +1,109 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2008 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#include "worm-helper.h"
+#include "ns3/worm-client.h"
+#include "ns3/uinteger.h"
+#include "ns3/names.h"
+
+namespace ns3 {
+
+
+WormClientHelper::WormClientHelper (Address address, uint16_t port)
+{
+  m_factory.SetTypeId (WormClient::GetTypeId ());
+  SetAttribute ("RemoteAddress", AddressValue (address));
+  SetAttribute ("RemotePort", UintegerValue (port));
+}
+
+WormClientHelper::WormClientHelper (Ipv4Address address, uint16_t port)
+{
+  m_factory.SetTypeId (WormClient::GetTypeId ());
+  SetAttribute ("RemoteAddress", AddressValue (Address(address)));
+  SetAttribute ("RemotePort", UintegerValue (port));
+}
+
+WormClientHelper::WormClientHelper (Ipv6Address address, uint16_t port)
+{
+  m_factory.SetTypeId (WormClient::GetTypeId ());
+  SetAttribute ("RemoteAddress", AddressValue (Address(address)));
+  SetAttribute ("RemotePort", UintegerValue (port));
+}
+
+void 
+WormClientHelper::SetAttribute (
+  std::string name, 
+  const AttributeValue &value)
+{
+  m_factory.Set (name, value);
+}
+
+void
+WormClientHelper::SetFill (Ptr<Application> app, std::string fill)
+{
+  app->GetObject<WormClient>()->SetFill (fill);
+}
+
+void
+WormClientHelper::SetFill (Ptr<Application> app, uint8_t fill, uint32_t dataLength)
+{
+  app->GetObject<WormClient>()->SetFill (fill, dataLength);
+}
+
+void
+WormClientHelper::SetFill (Ptr<Application> app, uint8_t *fill, uint32_t fillLength, uint32_t dataLength)
+{
+  app->GetObject<WormClient>()->SetFill (fill, fillLength, dataLength);
+}
+
+ApplicationContainer
+WormClientHelper::Install (Ptr<Node> node) const
+{
+  return ApplicationContainer (InstallPriv (node));
+}
+
+ApplicationContainer
+WormClientHelper::Install (std::string nodeName) const
+{
+  Ptr<Node> node = Names::Find<Node> (nodeName);
+  return ApplicationContainer (InstallPriv (node));
+}
+
+ApplicationContainer
+WormClientHelper::Install (NodeContainer c) const
+{
+  ApplicationContainer apps;
+  for (NodeContainer::Iterator i = c.Begin (); i != c.End (); ++i)
+    {
+      apps.Add (InstallPriv (*i));
+    }
+
+  return apps;
+}
+
+Ptr<Application>
+WormClientHelper::InstallPriv (Ptr<Node> node) const
+{
+  Ptr<Application> app = m_factory.Create<WormClient> ();
+  node->AddApplication (app);
+
+  return app;
+}
+
+} // namespace ns3
